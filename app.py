56eff0146e99ce39109a71bf042c935c85f73c8a
SMMotteler <sarahmotteler2021@gmail.com>
2021-07-21 15:39:31 +0000

    added image form

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
@@ -14,3 +14,10 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourimage', methods=['GET','POST'])
+def image():
+    userImageChoice=request.form['imagechoice']
+    my_image_url = getImageUrlFrom(userImageChoice)
+    #return "You got here! Yay!"+userImageChoice
+    return render_template("yourgif.html", time = datetime.now(), imgUrl=my_image_url)
--- model.py
+++ model.py
@@ -1,2 +1,6 @@
+import requests
+
 def getImageUrlFrom(query):
-    pass
+    my_nasa_api_response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera="+(query)+"&api_key=DEMO_KEY").json()
+    my_nasa_image_url = my_nasa_api_response["photos"][0]["img_src"]
+    return my_nasa_image_url
